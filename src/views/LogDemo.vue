e49b34e7921ceb4c10d3ccc0e577de00feee54c7
Shang <shang1219178163@gmail.com>
2022-03-25 18:26:39 +0800

    日志方法封装

--- src/views/LogDemo.vue
+++ src/views/LogDemo.vue
@@ -7,7 +7,10 @@
 </template>
 
 <script setup>
-import { ddlog, ddlogDemo } from "@/lib/ddlog.js";
+import { ddlog, ddlogExample, printInfo } from "@/lib/ddlog.ts";
+
+//引入工具类包，有一些帮助函数可以使用，这里主要使用的是格式化的部分。
+var util = require('util');
 
 const clickButton = () => {
   const greeting = "Hello,world";
@@ -26,9 +29,78 @@ const clickButton = () => {
 };
 
 const clickButton2 = () => {
-    ddlogDemo() 
+    ddlog("Hello,world", "a", "b", "c")
 
+    // ddlogExample() 
+    // console.warn("Hello,world", "a", "b", "c")
+
+    // let traceString = Error().stack
+    // const list = traceString.split("\n")
+    // console.log(list);
+  // log("Normal Logs");
+  // log("Warning Logs", Style.warning);
+  // log("Success Logs", Style.success);
+
+  printInfo("Hello,world", "a", "b", "c")
 };
+
+const Style = {
+  base: [
+    // "color: #fff",
+    // "background-color: #444",
+    "color: #000",
+    "padding: 2px 4px",
+    "border-radius: 2px"
+  ],
+  warning: [
+    // "color: #eee",
+    // "background-color: red"
+  ],
+  success: [
+    // "background-color: green"
+  ]
+}
+const log = (text, extra = []) => {
+  let style = Style.base.join(';') + ';';
+  style += extra.join(';'); // Add any additional styles
+  console.log(`%c${text}`, style);
+}
+
+// const printLog = () => {
+//     // ddlogDemo() 
+    
+//     let traceString = Error().stack
+//     console.log(traceString);
+
+//     const list = traceString.split("\n")
+//     console.log(list);
+
+//     // const str = "    at clickButton2 (webpack-internal:///./node_modules/cache-loader/dist/cjs.js?!./node_modules/babel-loader/lib/index.js!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader-v16/dist/index.js?!./src/views/LogDemo.vue?vue&type=script&setup=true&lang=js:35:25)"
+//     const matchFuncs = list[1].match(/at (\S*) \(/);
+//     if (matchFuncs == undefined || matchFuncs.length < 2) {
+//         console.log()
+//         return
+//     }
+//     // console.log("matchs", matchFuncs)
+//     const funcName = matchFuncs[1];
+//     console.log(`_${funcName}_`);
+
+//     const matchFiles = list[1].match(/[a-zA-Z0-9]*\.vue/);
+//     if (matchFiles == undefined || matchFiles.length == 0) {
+//         console.log()
+//         return
+//     }
+//     console.log("matchFiles", matchFiles);
+
+//     console.log(`${matchFiles[0]} ${funcName}: `)
+// };
+
+
+// var getStackTrace = function() {
+//   var obj = {};
+//   Error.captureStackTrace(obj, getStackTrace);
+//   return obj.stack;
+// };
 
 
 </script>
