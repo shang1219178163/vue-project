96554b55550315f894b230c2f6dece264e9fb450
shangbinbin <shangbinbin@haier.com>
2022-04-06 09:20:51 +0800

    解决ios audio无法自动播放、循环播放的问题

---
+++ src/views/VideoProblemDemo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+
+<script setup>
+import {getCurrentInstance, ref, reactive, onMounted, watch, defineProps} from 'vue';
+// import { Toast } from 'vant;
+
+// 解决ios audio无法自动播放、循环播放的问题
+// var music = document.getElementById(video);
+// var state = 0;
+
+// document.addEventListener(touchstart, function(){
+//     if(state==0){
+//         music.play();
+//         state=1;
+//     }
+// }, false);
+
+// document.addEventListener("WeixinJSBridgeReady", function () {
+//     music.play();
+// }, false);
+
+// //循环播放
+// music.onended = function () {
+//     music.load();
+//     music.play();
+// }
+</script>
+
+
+<style scoped lang='scss'>
+
+</style>
