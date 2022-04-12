80d6abf45fb4078dd8b6cac72b3a97d638c264bb
shangbinbin <shangbinbin@haier.com>
2022-04-12 09:13:17 +0800

    事件代理

---
+++ src/views/EventDelegateDemo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div>事件代理</div>
+        <ul @click="onClick">
+          <li class="li" v-for="(item, index) in 6" :data-index="index">
+            {{ item }}
+          </li>
+        </ul>
+    </div>
+</template>
+
+
+<script setup>
+import { ref, reactive, getCurrentInstance, onMounted, inject } from 'vue';
+// import { Toast } from 'vant;
+
+const list = reactive([])
+
+
+const onClick = (e) => {
+    if (e.target.nodeName.toLowerCase() === 'li') {
+        const index = parseInt(e.target.dataset.index)
+        // 获得引索后，只需要修改data数据就能改变UI了
+        doSomething(index)
+    }
+};
+
+const doSomething = (val) => {
+    console.log(val)
+};
+
+</script>
+
+
+<style scoped lang='scss'>
+
+.li{
+  margin: 10px;
+  background-color: green;
+}
+
+</style>
