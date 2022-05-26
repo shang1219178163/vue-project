ccd7f12219538feffc539e818425e4f591b2147b
shangbinbin <shangbinbin@haier.com>
2022-05-26 10:45:04 +0800

    格式化

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,26 +1,24 @@
-
-export function utility(){
-    return {
-        isArray:(o) => Object.prototype.toString.call(o) == "[object Array]",
-        isObj:(o) => Object.prototype.toString.call(o) == "[object Object]",
-        isNull:(o) => Object.prototype.toString.call(o) == "[object Null]",
-        isFunction:(o) => Object.prototype.toString.call(o) == "[object Function]",
-        isDate:(o) => Object.prototype.toString.call(o) == "[object Date]", 
-        isDocument:(o) => Object.prototype.toString.call(o) =="[object Document]"|| Object.prototype.toString.call(o) == "[object HTMLDocument]",
-        isNumber:(o) => Object.prototype.toString.call(o) == "[object Number]", 
-        isString:(o) => Object.prototype.toString.call(o) == "[object String]", 
-        isUndefined:(o) => Object.prototype.toString.call(o) == "[object Undefined]", 
-        isBoolean:(o) => Object.prototype.toString.call(o) == "[object Boolean]", 
-    }
-  }
-  
-export function isObj(o){
-    return Object.prototype.toString.call(o) == "[object Array]"
+export function utility() {
+  return {
+    isArray: (o) => Object.prototype.toString.call(o) == "[object Array]",
+    isObj: (o) => Object.prototype.toString.call(o) == "[object Object]",
+    isNull: (o) => Object.prototype.toString.call(o) == "[object Null]",
+    isFunction: (o) => Object.prototype.toString.call(o) == "[object Function]",
+    isDate: (o) => Object.prototype.toString.call(o) == "[object Date]",
+    isDocument: (o) => Object.prototype.toString.call(o) == "[object Document]" ||
+      Object.prototype.toString.call(o) == "[object HTMLDocument]",
+    isNumber: (o) => Object.prototype.toString.call(o) == "[object Number]",
+    isString: (o) => Object.prototype.toString.call(o) == "[object String]",
+    isUndefined: (o) => Object.prototype.toString.call(o) == "[object Undefined]",
+    isBoolean: (o) => Object.prototype.toString.call(o) == "[object Boolean]",
+  };
 }
 
 /// 类型检查
 export function checkType(o, type) {
-  const typeStr = Object.prototype.toString.call(o).replace(/\[object (\w+)\]/, "$1");
+  const typeStr = Object.prototype.toString
+    .call(o)
+    .replace(/\[object (\w+)\]/, "$1");
   return type ? type === (typeStr || typeStr.toLowerCase()) : typeStr;
 }
 // export function isArray(o){
