db890811f65c64ab21a0dc9c87029e382c9ff36d
shangbinbin <shangbinbin@haier.com>
2022-05-30 17:07:49 +0800

    css 箭头

--- src/views/CSSTriangle.vue
+++ src/views/CSSTriangle.vue
@@ -1,13 +1,36 @@
 <template>
   <div>
-    <p class="arrow_top"></p>
-    <br />
-    <p class="arrow_right"></p>
-    <br />
-    <p class="arrow_bottom"></p>
-    <br />
-    <p class="arrow_left"></p>
-    <br /><br />
+    <h1>css 箭头</h1>
+    <p class="button__arrow--top"></p>
+    <p class="button__arrow--right"></p>
+    <p class="button__arrow--bottom"></p>
+    <p class="button__arrow--left"></p>
+
+    <div style="border: 0.3px solid #e4e4e4"></div>
+    <h1>button__arrow </h1>
+    <!-- arrow button -->
+    <button class="button">
+        top
+        <div class="button__arrow--top"></div>
+    </button>
+
+    <!-- Right arrow button -->
+    <button class="button">
+        right
+        <div class="button__arrow--right"></div>
+    </button>
+
+    <!-- Down arrow button -->
+    <button class="button">
+        <div class="button__arrow--bottom"></div>
+        bottom
+    </button>
+
+    <!-- Left arrow button -->
+    <button class="button">
+        <div class="button__arrow--left"></div>
+        left
+    </button>
   </div>
 </template>
 
@@ -17,21 +40,30 @@
 
 <style scoped lang="scss">
 
+.button {
+    /* Center the content */
+    align-items: center;
+    display: flex;
+    justify-content: center;
 
-.arrow_top {
-  @include triangle;
+    gap: 8px;
+    /* Spacing */
+    padding: 8px;
+}
+.button__arrow--top {
+  @include arrowTop;
 }
 
-.arrow_right {
-  @include triangle($deg: 90deg);
+.button__arrow--right {
+  @include arrowRight;
 }
 
-.arrow_bottom {
-  @include triangle($deg: 180deg);
+.button__arrow--bottom {
+  @include arrowDown;
 }
 
-.arrow_left {
-  @include triangle($deg: 270deg);
+.button__arrow--left {
+  @include arrowLeft;
 }
 
 </style>
