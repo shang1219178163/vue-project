d0f08e2cd4e7da95866b83b13adceb462737695a
shangbinbin <shangbinbin@haier.com>
2022-05-30 16:11:07 +0800

    新增三角形 css 实现

---
+++ src/views/CSSTriangle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p class="arrow_top"></p>
+    <br />
+    <p class="arrow_right"></p>
+    <br />
+    <p class="arrow_bottom"></p>
+    <br />
+    <p class="arrow_left"></p>
+    <br /><br />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+
+
+.arrow_top {
+  @include triangle;
+}
+
+.arrow_right {
+  @include triangle($deg: 90deg);
+}
+
+.arrow_bottom {
+  @include triangle($deg: 180deg);
+}
+
+.arrow_left {
+  @include triangle($deg: 270deg);
+}
+
+</style>
