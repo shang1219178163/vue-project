a23858e032e65ce9b2e2d5afac01b981b48bbd97
shangbinbin <shangbinbin@haier.com>
2022-06-06 10:05:28 +0800

    代码重构

--- src/views/URLSearchParamsTest.vue
+++ src/views/URLSearchParamsTest.vue
@@ -4,8 +4,6 @@
     <button @click="parseURLString">button</button>
     <h2>链接</h2>
     <h3>{{urlString}}</h3>
-    <h2>解析参数:</h2>
-    <h3 v-for="(item, index) in list" :key="index">{{item[0]}}: {{item[1]}}</h3>
     <h2>URL对象:</h2>
     <h4 v-for="(item, index) in items" :key="index">{{item[0]}}: {{item[1]}}</h4>
   </div>
@@ -20,21 +18,21 @@ const urlString =ref(`https://sybird-oss.haier.net/resource/decorationMoney/inde
 ?token=d04eedfeb76d4e9bb1021eff8149c196&phone=18792710637&#/installment`)
 ;
 
-let list = reactive([]);
 let items = reactive([]);
 
 const parseURLString = async () => {
-  list = items.splice(items.length, 0);
   items = items.splice(items.length, 0);
 
   // const URL = new url.parse(urlString, true);
   let url = new URL(urlString.value);
   console.log("host:", url);
-  // console.log("url.searchParams:", url.searchParams.toString());
-  for (let item of url.searchParams) {
-    console.log("item:", item[0], item[1]);
-    list.push(item);
-  }
+  // console.log("url.searchParams.toString:", url.searchParams.toString());
+  
+  /// 可迭代带向转对象
+  let params = Object.fromEntries(url.searchParams);
+  console.log("url.searchParams:", params);
+  console.table([params]);
+
 
 `  host: URL {
 hash: "#/installment"
@@ -50,16 +48,6 @@ search: "?token=d04eedfeb76d4e9bb1021eff8149c196&phone=18792710637&"
 searchParams: URLSearchParams {}
 username: ""
 `
-  // const params = {};
-
-  // const searchParams = new URLSearchParams(url.search);
-  // console.log("searchParams:", searchParams);
-  // for (let p of searchParams) {
-  //   // console.log(p[0], p[1]);
-  //   list.push(p)
-  // }
-  // console.log("params:", params);
-  // console.log(typeof url, url);
 
   for(let key in url){ 
     // console.log(key, url[key]);
