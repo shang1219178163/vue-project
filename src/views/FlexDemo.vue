0dcd5e35e87cc42615ccbf7ecc2316b09b6f56bc
Shang <shang1219178163@gmail.com>
2022-07-02 08:15:19 +0800

    update

--- src/views/FlexDemo.vue
+++ src/views/FlexDemo.vue
@@ -1,26 +1,35 @@
 <template>
-  <div class='container'>
-    <ol id='ol'>
+  <!-- <div class="container">
+    <ol id="ol">
       <li>
-        <div class='cell' @click='tap'>
-          <div class='name'>样式1</div>
+        <div class="cell" @click="tap">
+          <div class="name">样式1</div>
           <flex1>flex1</flex1>
         </div>
       </li>
       <li>
-        <div class='cell' @click='tap'>
-          <div class='name'>样式2</div>
+        <div class="cell" @click="tap">
+          <div class="name">样式2</div>
           <flex2>flex2</flex2>
         </div>
       </li>
     </ol>
+  </div> -->
+  <div class="box">
+    <div class="column">
+      <span class="item"></span>
+      <span class="item"></span>
+    </div>
+    <div class="column">
+      <span class="item"></span> 
+      <span class="item"></span>
+    </div>
   </div>
-
 </template>
 
 <script setup>
-import flex1 from '@/components/Flex/Flex_1.vue'
-import flex2 from '@/components/Flex/Flex_2.vue'
+import flex1 from "@/components/Flex/Flex_1.vue";
+import flex2 from "@/components/Flex/Flex_2.vue";
 
 // const list = [
 //         {name: '样式1', tag: 'grid1'},
@@ -28,9 +37,8 @@ import flex2 from '@/components/Flex/Flex_2.vue'
 //       ]
 
 const tap = () => {
-      alert('tap')
-    }
-
+  alert("tap");
+};
 </script>
 
 <style scoped>
@@ -54,7 +62,7 @@ li {
   border-bottom: 0.0625rem solid;
 }
 
-.container{
+.container {
   display: flex;
   flex-direction: column;
   width: 100vw;
@@ -65,9 +73,27 @@ li {
 .cell {
   background-color: #4ba946;
   /* display: flex; */
-  width: 100vw - 8px*2;
+  width: 100vw - 8px * 2;
   margin: 0 0.5rem 0 0.5rem;
   align-items: center;
 }
 
+.box {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: space-between;
+
+  background-color: green;
+}
+.column {
+  flex-basis: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+.item{
+  margin: 8px;
+  width: 10px;
+  height: 10px;
+  background-color: red;
+}
 </style>
