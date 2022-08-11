94850ba7ff729370e330939b6466bf45c1d85a03
shangbinbin <shangbinbin@haier.com>
2022-08-11 15:54:15 +0800

    customRef 实现防抖

---
+++ src/utils/debouncedRef.js
@@ -0,0 +1,29 @@
+import { customRef } from 'vue'
+
+export function useDebouncedRef(value, delay = 200) {
+  let timeout;
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track();
+        return value;
+      },
+      set(newValue) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          value = newValue;
+          trigger();
+        }, delay);
+      }
+    }
+  })
+}
+
+{/* <script setup>
+import { useDebouncedRef } from './debouncedRef'
+const text = useDebouncedRef('hello')
+</script>
+
+<template>
+  <input v-model="text" />
+</template> */}
