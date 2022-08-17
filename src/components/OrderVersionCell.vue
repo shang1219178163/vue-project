0e97922f7bc168ad22e9c49674fc65529c515cf2
shangbinbin <shangbinbin@haier.com>
2022-08-17 18:39:42 +0800

    update

--- src/components/OrderVersionCell.vue
+++ src/components/OrderVersionCell.vue
@@ -1,88 +1,76 @@
-
-/**
- * 左右插槽 cell,
- ****************************************************
- * slot name="left"   slot name="right"*
- ****************************************************
- */
+/** * 左右插槽 cell, **************************************************** * slot
+name="left" slot name="right"* **************************************************** */
 <template>
-    <div class="content" @click="click">
-        <div class="left" >
-            <slot name="left">
-                <VButtonIcon 
-                class="button-icon" 
-                :title="tolength(title, titleLength)"
-                >
-                    <template #left>
-                        <van-image 
-                        :src="require('@/assets/images/icon_district.png')" 
-                        width="18px" 
-                        height="18px" 
-                        />
-                    </template>
-                </VButtonIcon>
-                <VButtonIcon 
-                class="button-icon" 
-                :title="tolength(subtitle, subtitleLength)"
-                >
-                    <template #left>
-                        <van-image 
-                        :src="require('@/assets/images/icon_manager.png')" 
-                        width="18px" 
-                        height="18px" 
-                        />
-                    </template>
-                </VButtonIcon>
-            </slot>
-        </div>
-        <div class="right">
-            <slot name="right">
-                <img
-                v-show="isSelected" 
-                class="cell-check"
-                style="width: 18px; height: 18px;" 
-                :src="require('@/assets/images/icon_checked.png')" 
-                />     
-            </slot>
-        </div>
+  <div class="content" @click="click">
+    <div class="left">
+      <slot name="left">
+        <VButtonIcon class="button-icon" :title="tolength(title, titleLength)">
+          <template #left>
+            <van-image
+              :src="require('@/assets/images/icon_district.png')"
+              width="18px"
+              height="18px"
+            />
+          </template>
+        </VButtonIcon>
+        <VButtonIcon class="button-icon" :title="tolength(subtitle, subtitleLength)">
+          <template #left>
+            <van-image
+              :src="require('@/assets/images/icon_manager.png')"
+              width="18px"
+              height="18px"
+            />
+          </template>
+        </VButtonIcon>
+      </slot>
     </div>
+    <div class="right">
+      <slot name="right">
+        <img
+          v-show="isSelected"
+          class="cell-check"
+          style="width: 18px; height: 18px"
+          :src="require('@/assets/images/icon_checked.png')"
+        />
+      </slot>
+    </div>
+  </div>
 </template>
 
-
 <script setup>
-import { getCurrentInstance, ref, reactive, watch, onMounted, } from 'vue';
+import {getCurrentInstance, ref, reactive, watch, onMounted} from "vue";
 // import { Toast } from 'vant;
 
 const props = defineProps({
-    isSelected: {
-        type: Boolean,
-        default: false
+  isSelected: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: "-",
+  },
+  subtitle: {
+    type: String,
+    default: "-",
+  },
+  titleLength: {
+    type: Number,
+    default: 4,
+    validator: (value) => {
+      return value >= 1;
     },
-    title: {
-        type: String,
-        default: "-",
+  },
+  subtitleLength: {
+    type: Number,
+    default: 4,
+    validator: (value) => {
+      return value >= 1;
     },
-    subtitle: {
-        type: String,
-        default: "-",
-    },
-    titleLength: {
-        type: Number,
-        default: 4,
-        validator: (value) => {
-            return value >= 1;
-        }
-    },
-    subtitleLength: {
-        type: Number,
-        default: 4,
-        validator: (value) => {
-            return value >= 1;
-        }
-    },
-})
+  },
+});
 
-const emit = defineEmits(['update:isSelected', "click"]);
+const emit = defineEmits(["update:isSelected", "click"]);
 
 // const isSelectedVal = computed({
 //   set: (newVal) => {
@@ -94,39 +82,34 @@ const emit = defineEmits(['update:isSelected', "click"]);
 // });
 
 const click = () => {
-    console.log("VPopverCell", props.isSelected);
-    emit('click', props);
+  console.log("VPopverCell", props.isSelected);
+  emit("click", props);
 };
-
 
 const tolength = (val, count = Number.MAX_VALUE, prefix = "") => {
-  if (val == undefined) return "-"
+  if (val == undefined) return "-";
   return val.length < count ? val : val.substring(0, count) + prefix;
 };
-
 </script>
 
-
-<style scoped lang='scss'>
-
+<style scoped lang="scss">
 // div{
 //     border: 0.5px solid blue;
 // }
 
-.content{
-    display: flex;
-    justify-content: space-between;
+.content {
+  display: flex;
+  justify-content: space-between;
 
-    height: 44px;
-    padding-right: 12px;
+  height: 44px;
+  padding-right: 12px;
 
-    border-bottom: 1px solid red;
+  border-bottom: 1px solid red;
 }
 
-
-.left{
-    @include  flex-center;
-    justify-content: space-evenly;
+.left {
+  @include flex-center;
+  justify-content: space-evenly;
 }
 
 .divider {
@@ -134,11 +117,11 @@ const tolength = (val, count = Number.MAX_VALUE, prefix = "") => {
   height: 0.5px;
   background-color: #eeeeee;
   background-color: red;
-//   margin: 0 20px;
+  //   margin: 0 20px;
 }
 
-.right{
-    @include  flex-center;
-    align-self: center;
+.right {
+  @include flex-center;
+  align-self: center;
 }
 </style>
