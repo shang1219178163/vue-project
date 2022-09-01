25397304a4b1b428a049cc8d47e4a8018320ecec
shangbinbin <shangbinbin@haier.com>
2022-09-01 12:07:39 +0800

    vueuse

---
+++ src/views/useGeolocationDemo.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <h1>{{ $route.meta.title }}</h1>
+  <pre>{{
+   coordsInfo
+  }}</pre>
+
+  <!-- <div>{{ coordsInfo }}</div> -->
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+import { useGeolocation } from '@vueuse/core';
+
+const { coords, locatedAt, error } = useGeolocation();
+
+const coordsInfo = ref(JSON.stringify(
+    {
+      coords: {
+        accuracy: coords.accuracy,
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        altitude: coords.altitude,
+        altitudeAccuracy: coords.altitudeAccuracy,
+        heading: coords.heading,
+        speed: coords.speed,
+      },
+      locatedAt,
+      error: error ? error.message : error,
+    },
+    null,
+    2,
+  ));
+
+</script>
---
+++ src/views/useNetworkDemo.vue
@@ -0,0 +1,24 @@
+
+
+<template>
+  <pre lang="yaml">{{ text }}</pre>
+  <div>{{ text }}</div>
+  <div 
+    v-for="(value, key) in network" :key="key"
+  >
+  {{ key }}:    {{value ?? '-'}}
+  </div>
+</template>
+
+
+<script setup>
+import { reactive, toRaw } from 'vue';
+import { useNetwork } from '@vueuse/core';
+
+const network = reactive(useNetwork());
+const text = JSON.stringify(network);
+
+console.log('network', network);
+
+console.log('network.keys', Object.keys(toRaw(network)));
+</script>
