84fcd428819b08f9849d991c43d9975d156be240
shang1219178163 <shang1219178163@gmail.com>
2022-11-14 08:40:08 +0800

    新增

---
+++ src/views/TestPageHeight.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="page">
+    <navbar
+      isleftarrow
+      navBarTitle="热门活动" 
+      closeWebview
+    >
+    </navbar> 
+    <div class="page-list">
+      list
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import navbar from '@/components/navbar.vue';
+
+import { getCurrentInstance, ref, reactive, watch, onMounted, } from 'vue';
+// import { Toast } from 'vant';
+import { useRouter, useRoute } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+
+onMounted(() => {
+  onRefresh();
+});
+
+watch(() => route.params, (params, previousParams) => {
+  if (Object.keys(params).length === 0 ) {
+    return;
+  }
+  handleRouteParams();
+});
+
+const handleRouteParams = () => {
+
+};
+
+
+const onRefresh = async () => {
+
+};
+
+const onMore = async () => {
+
+};
+
+</script>
+
+
+<style scoped lang='scss'>
+
+.page{
+  border: 10px solid blue;
+
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.page > div:last-child {
+  background-color: tomato;
+  flex: 1;
+}
+</style>
