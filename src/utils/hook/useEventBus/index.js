ce271c884938cbee0dfac61cd85a244fd3b45f5f
shang1219178163 <shang1219178163@gmail.com>
2022-11-15 15:02:02 +0800

    更新

--- src/utils/hook/useEventBus/index.js
+++ src/utils/hook/useEventBus/index.js
@@ -2,46 +2,46 @@ import {onUnmounted} from 'vue'
 
 class EventBus {
     constructor() {
-        this.eventMap = new Map()
+        this.eventMap = new Map();
     }
 
     on(key, fn) {
-        let handlers = this.eventMap.get(key) ?? []
-        handlers.push(fn)
-        this.eventMap.set(key, handlers)
+        let handlers = this.eventMap.get(key) ?? [];
+        handlers.push(fn);
+        this.eventMap.set(key, handlers);
     }
 
     off(key, fn) {
-        const handlers = this.eventMap.get(key)
-        if (!handlers) return
+        const handlers = this.eventMap.get(key);
+        if (!handlers) return;
         if (fn) {
-            const idx = handlers.indexOf(fn)
-            idx > -1 && handlers.splice(idx, 1)
-            this.eventMap.set(key, handlers)
+            const idx = handlers.indexOf(fn);
+            idx > -1 && handlers.splice(idx, 1);
+            this.eventMap.set(key, handlers);
         } else {
-            this.eventMap.delete(key)
+            this.eventMap.delete(key);
         }
     }
 
     once(key, fn) {
         const handlers = [(payload) => {
-            fn(payload)
-            this.off(key)
+            fn(payload);
+            this.off(key);
         }]
-        this.eventMap.set(key, handlers)
+        this.eventMap.set(key, handlers);
     }
 
     emit(key, payload) {
-        const handlers = this.eventMap.get(key)
-        if (!Array.isArray(handlers)) return
+        const handlers = this.eventMap.get(key);
+        if (!Array.isArray(handlers)) return;
         handlers.forEach(handler => {
-            handler(payload)
+            handler(payload);
         })
     }
 }
 
 // 事件总线，全局单例
-const bus = new EventBus()
+const bus = new EventBus();
 
 export default function useEventBus() {
     let instance = {
@@ -53,26 +53,26 @@ export default function useEventBus() {
         clear() {
             this.eventMap.forEach((list, key) => {
                 list.forEach(fn => {
-                    bus.off(key, fn)
+                    bus.off(key, fn);
                 })
             })
-            eventMap.clear()
+            eventMap.clear();
         }
     }
-    let eventMap = new Map()
+    let eventMap = new Map();
     // 劫持两个监听方法，收集当前组件对应的事件
     const on = (key, fn) => {
-        instance.on(key, fn)
-        bus.on(key, fn)
+        instance.on(key, fn);
+        bus.on(key, fn);
     }
     const once = (key, fn) => {
-        instance.once(key, fn)
-        bus.once(key, fn)
+        instance.once(key, fn);
+        bus.once(key, fn);
     }
 
     // 组件卸载时取消相关的事件
     onUnmounted(() => {
-        instance.clear()
+        instance.clear();
     })
     return {
         on,
