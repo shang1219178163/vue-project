94e8c707514d0622085adc0659dc091490210bda
shang1219178163 <shang1219178163@gmail.com>
2022-11-17 16:08:22 +0800

    update

--- src/utils/common.js
+++ src/utils/common.js
@@ -14,6 +14,41 @@ function deepCopy(obj) {
     return objNew;
 }
 
+///自定义深拷贝
+const deepClone = (a, cache) => {
+  if(!cache){
+    cache = new Map() // 缓存不能全局，最好临时创建并递归传递
+  }
+  if(a instanceof Object) { // 不考虑跨 iframe
+    if(cache.get(a)) { return cache.get(a) }
+    let result 
+    if(a instanceof Function) {
+      if(a.prototype) { // 有 prototype 就是普通函数
+        result = function(){ return a.apply(this, arguments) }
+      } else {
+        result = (...args) => { return a.call(undefined, ...args) }
+      }
+    } else if(a instanceof Array) {
+      result = []
+    } else if(a instanceof Date) {
+      result = new Date(a - 0)
+    } else if(a instanceof RegExp) {
+      result = new RegExp(a.source, a.flags)
+    } else {
+      result = {}
+    }
+    cache.set(a, result)
+    for(let key in a) { 
+      if(a.hasOwnProperty(key)){
+        result[key] = deepClone(a[key], cache) 
+      }
+    }
+    return result
+  } else {
+    return a
+  }
+}
+
 // 该方法用于将一个数字按照指定位进行四舍五入：
 const round = (n, d) => Number(Math.round(n + "e" + d) + "e-" + d)
 // round(1.005, 2) //1.01
