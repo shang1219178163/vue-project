5b4b5656ce225a84bda38addcf28c9cd400f77fd
Shang <shang1219178163@gmail.com>
2023-05-14 18:00:35 +0800

    更新

--- src/views/DialogDemo.vue
+++ src/views/DialogDemo.vue
@@ -1,15 +1,17 @@
 <template>
   <h1>{{ $route.meta.title }}</h1>
   <!-- <h1>{{ JSON.stringify(route) }}</h1> -->
-  <button id="btn-open" @click="onDialogShow">打开 dialog</button>
 
-  <dialog id="dialog">
+
+  <dialog id="d">
     <form method="dialog">
+      <p>I'm a dialog!</p>
       <input type="text">
       <button type="submit" value="foo">提交</button>
     </form>
-    <!-- <button id="btn-close" @click="onDialogClose">关闭</button> -->
   </dialog>
+  <button id="btn-open" @click="onDialogShow">打开 dialog</button>
+  <button @click="d.showModal()">showModal()</button>
 </template>
 
 
@@ -25,20 +27,28 @@ const route = useRoute();
 onMounted(() => {
   let btnOpen = document.querySelector("#btn-open");
   let btnClose = document.querySelector("#btn-close");
-  let dialog = document.querySelector("#dialog");
+  let dialog = document.querySelector("#d");
 
   btnOpen.addEventListener("click", function () {
-  // dialog.show();
-  dialog.showModal();
+    dialog.showModal();
+    openCheck(dialog);
   });
 
 
   btnClose.addEventListener("click", function () {
     dialog.close();
+    openCheck(dialog);
   });
 
 });
 
+function openCheck(dialog) {
+  if (dialog.open) {
+    console.log("Dialog open");
+  } else {
+    console.log("Dialog closed");
+  }
+}
 
 // /// 打开弹窗
 // const onDialogShow = () => {
@@ -55,7 +65,12 @@ onMounted(() => {
 
 
 <style scoped lang='scss'>
-.dialog::backdrop {
+#btn-open{
+  padding: 8px;
+  border-radius: 4px;
+}
+
+.d::backdrop {
   /* make the backdrop a semi-transparent black */
   background-color: rgba(0, 0, 0, 0.7);
 }
